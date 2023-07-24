2eda7593223bdc38f2b3d1b9289becc24975bbf8
Vladyslav Bilous <vladeezym@gmail.com>
2023-07-24 06:21:02 +0300

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,35 +1,37 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-  testDir: './tests/specs/',
-  outputDir: 'reports/test-results/',
-  fullyParallel: false,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+    testDir: './tests/specs/',
+    outputDir: 'reports/test-results/',
+    fullyParallel: false,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
 
-  reporter: [
-    ['list', { printSteps: true }],
-    [
-      'html',
-      {
-        open: 'never',
-        outputFolder: 'docs/',
-      },
-    ],
-  ],
+    reporter: process.env.CI
+        ? [['junit', { outputfile: 'results.xml' }]]
+        : [
+              ['list', { printSteps: true }],
+              [
+                  'html',
+                  {
+                      open: 'never',
+                      outputFolder: 'docs/',
+                  },
+              ],
+          ],
 
-  use: {
-    baseURL: 'https://www.saucedemo.com/',
-    viewport: { width: 1920, height: 1080 },
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-  },
-
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    use: {
+        baseURL: 'https://www.saucedemo.com/',
+        viewport: { width: 1920, height: 1080 },
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',
     },
-  ],
-});
+
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+    ],
+})
